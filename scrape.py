565ee533285df929c2de5c29c9b9724d6b359e0d
RaphaS9 <rsousa059@gmail.com>
2022-09-16 00:16:53 -0300

    Primeiro commit

--- scrape.py
+++ scrape.py
@@ -9,13 +9,13 @@ sheet.title = 'Top Rated Movies'
 sheet.append(["Movie Rank", "Movie Name", "Year of Release", "IMDB Rating"])
 
 try:
+    #definindo a fonte para nosso "raspamento" de dados
     source = requests.get("https://www.imdb.com/chart/top/")
     source.raise_for_status()
-
     soup = BeautifulSoup(source.text, "html.parser")
 
+    #definindo quais elemento devo procurar na página
     movies = soup.find("tbody", class_="lister-list").find_all("tr")
-
     for movie in movies:
         title = movie.find("td", class_="titleColumn").a.text
         rank = movie.find("td", class_="titleColumn").get_text(strip=True).split(".")[0]
